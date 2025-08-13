0223e0b1e806dfb1717dac361ef6d2c1954d4c11
JackFrostica <jadams@cognarixai.org>
2025-08-13 15:23:38 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Cognarix AI</h1>
+      <p>This is your starting point.</p>
+    </div>
+  );
+}
